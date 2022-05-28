c0d882a13cc8c23565a0ad8c461bb61c0e1d8503
Ahmed Essam <aessam1306@gmail.com>
2022-05-28 14:04:24 +0200

    Edit seeder

--- database/seeders/AchievementSeeder.php
+++ database/seeders/AchievementSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\AchievementTypes;
 use App\Models\Achievement;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,61 +19,61 @@ class AchievementSeeder extends Seeder
         DB::table('achievements')->insert([
             'title' => 'First Lesson Watched',
             'number' => 1,
-            'type' => Achievement::LESSON,
+            'type' => AchievementTypes::Lesson,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '5 Lesson Watched',
             'number' => 5,
-            'type' => Achievement::LESSON,
+            'type' => AchievementTypes::Lesson,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '10 Lesson Watched',
             'number' => 10,
-            'type' => Achievement::LESSON,
+            'type' => AchievementTypes::Lesson,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '25 Lesson Watched',
             'number' => 25,
-            'type' => Achievement::LESSON,
+            'type' => AchievementTypes::Lesson,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '50 Lesson Watched',
             'number' => 50,
-            'type' => Achievement::LESSON,
+            'type' => AchievementTypes::Lesson,
         ]);
 
         DB::table('achievements')->insert([
             'title' => 'First Comment Written',
             'number' => 1,
-            'type' => Achievement::COMMENT,
+            'type' => AchievementTypes::Comment,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '3 Comments Written',
             'number' => 3,
-            'type' => Achievement::COMMENT,
+            'type' => AchievementTypes::Comment,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '5 Comments Written',
             'number' => 5,
-            'type' => Achievement::COMMENT,
+            'type' => AchievementTypes::Comment,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '10 Comments Written',
             'number' => 10,
-            'type' => Achievement::COMMENT,
+            'type' => AchievementTypes::Comment,
         ]);
 
         DB::table('achievements')->insert([
             'title' => '20 Comments Written',
             'number' => 20,
-            'type' => Achievement::COMMENT,
+            'type' => AchievementTypes::Comment,
         ]);
     }
 }
